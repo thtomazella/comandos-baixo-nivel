cd274a9010b0a722e2c292463fdf4a8419aab69d
Thiago Tomazella <88443010+thtomazella@users.noreply.github.com>
2022-04-20 15:57:30 -0300

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* main é um ponto de entrada do programa, como _start em Assembly
+ * main devolve o codigo de retorno que por sua vez é atribuido
+ * a chamada de sistema exit
+ * A palavra main não aceita argumento*/
+int main(void){
+  /* vaiável em main será destruida assim que o main for finalizado*/
+  int x = 43;
+  int = y;
+  y = x -1
+  printf("Hello, world! y=%d x=%d\n", y,x);
+  return 0;
+}
+ 
