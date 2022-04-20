c3428fdf7a2fc76bba7171c0e538427a4c002ddc
Thiago Tomazella <88443010+thtomazella@users.noreply.github.com>
2022-04-20 15:11:44 -0300

    Update hello.asm

--- hello.asm
+++ hello.asm
@@ -3,8 +3,8 @@ section .data
 message: 'hello, wordl!', 10
 section .text
 _start:
-  mov rax, 1 
-  mov rdi, 1
-  mov rsi, message
-  mov rdx, 14
-  syscall
+  mov rax, 1          ; Numero de chamada de sistema deve ser armazendado em rax
+  mov rdi, 1          ; argumento #1 em rdi: onde escrever
+  mov rsi, message    ; argumento #2 em rsi: onde começa a string
+  mov rdx, 14         ; argumento #3 em rdx: quantos bytes deve ser escritos   
+  syscall             ; faz uma chamada de sistema
